1ac98d9d33f5d1dfe9069dc3dad687822e79002f
Rafael331g <180237442+Rafael331g@users.noreply.github.com>
2026-08-21 00:38:33 -0300

    Set Menu in begning of project

--- code/game.py
+++ code/game.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# import de pygame
+import pygame
+
+# Chamando o codigo Menu
+from Code.const import WIN_WIDTH, WIN_HEIGHT
+from Code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        # comando para iniciar o py game
+        pygame.init()
+
+        # inicializa a janela que iria mostrar o jogo ou algum grafico
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
-        pass
+
+        # LOOP para manter a janela aberta
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from Code.game import Game
 
-pygame.init()
-
-window = pygame.display.set_mode(size=(600, 480))
-
-print('setup end')
-print('loop start')
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Para fechar a janela
-            quit()  #Encerra o Pygame 
+Game = Game()
+Game.run()
